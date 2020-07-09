df66b57b95d2233c0ebeb3457ab395af7b5b2ffb
user1 <user1@localhost.localdomain>
2020-07-09 05:02:49 -0400

    updated Readme file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # This Shell script will automate the process of Installing the following: 
-# Compilation of Python3 and Sqlite3 is nessesary because django 3.06 requires Sqlite3 version 3.25.0 (verification needed) and above, which in turn requires Python version 3.8.0 (verification needed) and above. 
-	#Python3 version 3.8.3
-	#Django version 3.06 
-	#Sqlite3 version 3.32.1 
+#	Python3 version 3.8.3
+#	Sqlite3 version 3.32.1
+# Compilation of Python3 and Sqlite3 is nessesary because django 3.06 requires Sqlite3 version 3.25.0 and above, which in turn requires Python version 3.8.0 and above. Normally this wouldnt be a problem and could be handled by installing latest binaries from yum, but they are not available for CentOS 7. 
+
+
+
 
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "-->Updating CentOS 7 System"
+yum -y update 
+
+
+echo "-->Installing wget" 
+yum -y install wget 
+
+echo "Installing development tools  "
+yum -y install openssl-devel bzip2-devel libffi-devel
+
+echo "Downloading sqlite3"
+wget https://www.sqlite.org/2020/sqlite-autoconf-3320100.tar.gz
+
+echo "unzip sqlite3"
+tar zxvf sqlite-autoconf-3320100.tar.gz
+cd sqlite-autoconf-3320100
+./configure
+
+echo "archive old binary of sqlite3 if it exist"  
+if [ -f /usr/bin/sqlite3 ]; then 
+	echo "Archiving old version of sqlite3"
+	mv /usr/bin/sqlite3 /usr/bin/sqlite3.bak
+fi
+
+
+make
+make install  
+
+sudo echo "export LD_LIBRARY_PATH="/usr/local/lib" ">> ~/.bashrc 
+sudo echo "export LD_LIBRARY_PATH="/usr/local/lib" ">> ~/.bash_profile 
+
+echo "Installing Python 3.8.0"
+cd ..
+wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
+tar zxvf Python-3.8.3.tgz
+cd Python-3.8.3/
+./configure 
+make 
+make install 
+
+
